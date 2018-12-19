21b7ef36bf1058501019decccd99b8d82f55f0e2
tinpay <sfukui@gmail.com>
2018-12-19 16:27:07 +0900

    init

---
+++ SubSampleCommon/Common/APIClient.swift
@@ -0,0 +1,22 @@
+//
+//  APIClient.swift
+//  Common
+//
+//  Created by cw-fukui on 2018/12/09.
+//  Copyright © 2018年 fukui. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+public class APIClient {
+    public static let shared = APIClient()
+    
+    public func send() {
+        
+        
+        print("request send. \(AlamofireVersionNumber)")
+        
+        print("")
+    }
+}
---
+++ SubSampleCommon/Common/Common.h
@@ -0,0 +1,19 @@
+//
+//  Common.h
+//  Common
+//
+//  Created by cw-fukui on 2018/12/09.
+//  Copyright © 2018年 fukui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Common.
+FOUNDATION_EXPORT double CommonVersionNumber;
+
+//! Project version string for Common.
+FOUNDATION_EXPORT const unsigned char CommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Common/PublicHeader.h>
+
+
